64311436ee5aa06a3d2e6e44cbe6257474ffec97
Sailaja Nagireddy <sailaja-nagireddy@users.noreply.github.com>
2020-11-08 08:17:11 +0530

    Added a mock counter component to averx-vue.

---
+++ projects/averx-vue/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+<div id="counter">
+    Counter: {{ counter }}
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    props: {
+        counter: String
+    }
+}
+</script>
